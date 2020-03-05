05b9b4c72f014c3f41c68ca167504e583af80cbf
Yoshiki <32304200+yoshikir@users.noreply.github.com>
2020-03-05 11:36:26 +0100

    Initial commit (1)

---
+++ gitlfsAuto.sh
@@ -0,0 +1,7 @@
+git lfs install
+git lfs track "*.psd"
+git lfs track "*.mp4"
+git lfs track "*.mpeg"
+git lfs track "*.wav"
+git lfs track "*.asset"
+echo "Added .wav , .mpeg, .mp4, .psd. You can close this window now."
